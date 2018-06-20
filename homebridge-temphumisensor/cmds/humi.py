aa546e7a95d9d38d5f74447a1e477dc16a484a15
Y.N <s1240095@gmail.com>
2018-06-20 22:17:16 +0900

    Add sensor reader python code.

---
+++ homebridge-temphumisensor/cmds/humi.py
@@ -0,0 +1,22 @@
+import sys
+humi = 50
+print("%d" % humi, end="")
+sys.exit()
+
+
+
+
+import RPi.GPIO as GPIO
+import dht11
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin=24)
+
+result = instance.read()
+if result.is_valid():
+	print("%d" % result.humidity, end="")
