8c7d5dad7bad684efeddb24ef47a5e7b825b69a8
Y.N <s1240095@gmail.com>
2018-06-20 11:25:56 +0900

    Add index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+var service, chara
+
+module.exports = function (homebridge) {
+  service = homebridge.hap.Service
+  chara = homebridge.hap.Characteristic
+  homebridge.registerAccessory('homebridge-test','test',testAcc)
+}
+
+function testAcc (log,config){
+  this.log = log
+  this.swstatus = 0
+
+  this.testservice = new Service.Switch(this.name)
+}
+
+testAcc.prototype = {
+  identify: function (callback){
+    this.log('Identify!')
+    callback()
+  },
+
+  setState: function (value, callback) {
+    this.log('setState')
+    this.log('value:' + value)
+    this.swstatus = value
+    callback(null)
+  },
+
+  getState: function (callback) {
+    this.log('getState')
+    callback(null,this.swstatus)
+  },
+
+  getServices: function(){
+    this.log('setService')
+
+    var infosv = new service.AccessoryInformation()
+    infosv.setCharacteristic(Characteristic.Manufacturer,'Test Manufacture')
+          .setCharacteristic(Characteristic.Model,'Test Model')
+          .setCharacteristic(Characteristic.SerialNumber,'TEST1234')
+
+    this.testservice.getCharacteristic(Characteristic.On)
+          .on('set',this.setState.bind(this))
+          .on('get',this.getState.bind(this))
+
+    return [infosv,this.testservice]
+  }
+}
