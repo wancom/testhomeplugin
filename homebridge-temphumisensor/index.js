342505d2c67374909df3f4e871de7deca710de4a
Y.N <s1240095@gmail.com>
2018-06-23 00:24:44 +0900

    Improve value precision.

--- homebridge-temphumisensor/index.js
+++ homebridge-temphumisensor/index.js
@@ -29,7 +29,7 @@ THAcc.prototype = {
         callback(err)
         return
       }
-      callback(null,parseInt(stdout, 10))
+      callback(null,parseFloat(stdout, 10))
     })
   },
 
@@ -39,7 +39,7 @@ THAcc.prototype = {
         callback(err)
         return
       }
-      callback(null,parseInt(stdout, 10))
+      callback(null,parseFloat(stdout, 10))
     })
   },
 
