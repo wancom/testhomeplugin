0b7f124abae14bc75fd8bc7d278653311c9345b5
Y.N <s1240095@gmail.com>
2018-06-27 12:26:57 +0900

    Add pir.py

---
+++ homebridge-pirsensor/cmds/pir.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+
+sensor = 9
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+last = False
+
+while True:
+	stat = GPIO.input(sensor)
+	if stat != last:
+		if stat:
+			print("1",end="",flush=True)
+		else:
+			print("0",end="",flush=True)
+		last = stat
+	time.sleep(1)
